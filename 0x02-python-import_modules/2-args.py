d48d3ee303d7192ec1bd06bb1134e1070fa2786d
James-Koduah <jameskoduah171@gmail.com>
2023-01-06 11:47:27 -0800

    4

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,15 @@
 if __name__ == "__main__":
 
     import sys
-
+    b = 's'
+    s = ':'
     n = len(sys.argv)
-    print("{} arguments:".format(n-1))
+    if n == 1:
+        s = '.'
+    if n == 2:
+        b = ''
+
+
+    print("{} argument{}{}".format(n-1, b, s))
     for x in range(1, n):
         print("{}: {}".format(x, sys.argv[x]))
