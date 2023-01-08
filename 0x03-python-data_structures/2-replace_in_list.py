018836590b53734c98c36b71f4be4a603adb8a62
James-Koduah <jameskoduah171@gmail.com>
2023-01-08 14:02:42 -0800

    pycodestyle

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@ def replace_in_list(my_list, idx, element):
 
     a = len(my_list)
     if idx < 0:
-        return my_list 
+        return my_list
     if idx > (a - 1):
         return my_list
     my_list[idx] = element
