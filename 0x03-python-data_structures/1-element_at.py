0e6b79a333b5b5f2af08925d471706423c05e3a0
James-Koduah <jameskoduah171@gmail.com>
2023-01-08 13:52:13 -0800

    1-main.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+
+    a = len(my_list)
+    if idx < 0:
+        return None
+    if idx > (a - 1):
+        return None
+    return (my_list[idx])
