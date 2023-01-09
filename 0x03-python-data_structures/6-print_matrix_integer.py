f9a6ce291c75068a12e69f01388fc01a5f784ab1
James-Koduah <jameskoduah171@gmail.com>
2023-01-09 05:35:22 -0800

    code checker

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,9 +3,9 @@
 def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         i = 0
-        for a in x: 
-            print("{:d}".format(a), end='') 
+        for a in x:
+            print("{:d}".format(a), end='')
             if i != (len(x) - 1):
-                    print(" ", end="")
-            i+=1
+                print(" ", end="")
+            i += 1
         print("")
