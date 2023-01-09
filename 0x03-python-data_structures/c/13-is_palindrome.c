19b1996f0368e9bd8223a8bfc126c952827373f2
James-Koduah <jameskoduah171@gmail.com>
2023-01-09 11:43:55 -0800

    13-is_palindrome.c

---
+++ 0x03-python-data_structures/c/13-is_palindrome.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+void reverse_array(int ori[], int rev[], int len)
+{
+	int i;
+	int j = 0;
+
+	for (i = len - 1; i >= 0; i--)
+	{
+		rev[j] = ori[i];
+		j++;
+	}
+
+}
+int is_palindrome(listint_t **head)
+{
+	int store[1024];
+	int rep[1024];
+	listint_t *h;
+	int i = 0;
+	int is_pal = 1;
+
+	h = *head;
+	while (h != NULL)
+	{
+		store[i] = h->n;
+		h = h->next;
+		i++;
+	}
+
+	reverse_array(store, rep, i);
+	i--;
+	for (i; i >= 0; i--)
+	{
+		if (store[i] != rep[i])
+			is_pal = 0;
+		printf("ori: %d; dup: %d\n", store[i], rep[i]);
+	}
+
+
+	return (is_pal);
+}
