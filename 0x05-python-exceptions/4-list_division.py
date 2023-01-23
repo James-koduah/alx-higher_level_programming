6728072391e9c3ca513f12210a159dc3d2765b6d
James koduah <101557545+James-koduah@users.noreply.github.com>
2023-01-23 19:02:05 +0000

    range

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     final = []
-    for x in range(list_length):
+    for x in range(0, list_length):
         try:
             j = my_list_1[x] / my_list_2[x]
         except TypeError:
