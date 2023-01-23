2e5a88bf5e6858d7a1ee83c9372bb9ea3b7d0e0c
James-Koduah <jameskoduah171@gmail.com>
2023-01-23 06:39:38 -0800

    1-safe_print_integer.py 2

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    mm = True
     try:
         print("{:d}".format(value))
+        return (True)
     except ValueError:
-        mm = False
-    return mm
+        return (False)
