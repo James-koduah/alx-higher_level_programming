e02a12aa7c8984db322541c9ed89098fc7bcdea2
James-Koduah <jameskoduah171@gmail.com>
2023-01-30 10:02:46 -0800

    4-square.py dddddddd

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -21,10 +21,16 @@ class Square:
         """Return the area of the square"""
         return (self.__size * self.__size)
 
+    @property
     def size(self):
         """Return private variable"""
         return self.__size
-
+    
+    @size.setter
     def size(self, value):
-        """Set privat variable"""
+        """Set private variable"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
         self.__size = value
