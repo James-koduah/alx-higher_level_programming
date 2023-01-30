93920881d6e022e50f1d42bc23b3599fe9f7b8ae
James-Koduah <jameskoduah171@gmail.com>
2023-01-30 05:20:57 -0800

    1-square.py 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A Square class"""
+
+
+class Square:
+
+    def __init__(self, size):
+        """Instance of Square
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
