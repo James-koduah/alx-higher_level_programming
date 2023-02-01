15375cfd754d3e4a00f253b421a7f8acf62598dc
James-Koduah <jameskoduah171@gmail.com>
2023-02-01 00:09:06 -0800

    6-square.py 1

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""A Square class"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Instance of Square
+
+        Args:
+            size (int): size of square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+
+        if type(position) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(position[0]) != int or type(position[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif position[0] < 0 or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) != tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) != int or type(value[1]) != int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+
+
+
+    def area(self):
+        """Return the area of the square"""
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        """Return private variable"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set private variable"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """prints square with # character"""
+        x = self.size
+        y = self.position
+        if x == 0:
+            print("")
+        else:
+            for i in range(0, x):
+                for n in range(0, y[0]):
+                    print(" ", end="")
+                for r in range(0, x):
+                    print("#", end="")
+                print("")
