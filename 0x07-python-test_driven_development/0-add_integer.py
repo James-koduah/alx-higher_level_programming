3a7c9aa407c13e64194e9b9864a7f0d81a9499f8
James-Koduah <jameskoduah171@gmail.com>
2023-02-07 02:47:18 -0800

    0-add_integer.py pycodestyle

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
+"""Addition function"""
+
+
 def add_integer(a, b=98):
+    """Returns sum of a and b
+
+    Raises:
+        TypeError: if arguments are not int or floats
+    """
     if type(a) != int and type(a) != float:
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
