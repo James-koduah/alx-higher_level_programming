1c496e0ce3ca7deeff3e94dd3c3049657ee91350
James-Koduah <jameskoduah171@gmail.com>
2023-02-08 03:42:22 -0800

    7-add_item.py hope

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Take command line arguments and add them to a list"""
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 import sys
 
-def add_args_to_list():
+if __name__ == "__main__":
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
     """Adds items to list form a file"""
     b = "add_item.json"
     c = []
@@ -13,11 +14,9 @@ def add_args_to_list():
             c = load_from_json_file(b)
         except:
             save_to_json_file(c, b)
-        return
 
     for i in range(1, len(sys.argv)):
         c = load_from_json_file(b)
         c.append(sys.argv[i])
         save_to_json_file(c, b)
 
-add_args_to_list()
