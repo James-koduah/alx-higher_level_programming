a804e3e850a7c7cfc22ae8bdebfc790b7e9e23ff
James-Koduah <jameskoduah171@gmail.com>
2023-02-08 06:15:50 -0800

    6-base_geometry.py first

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Empyt class"""
+
+
+class BaseGeometry():
+    """Nothing"""
+    
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
