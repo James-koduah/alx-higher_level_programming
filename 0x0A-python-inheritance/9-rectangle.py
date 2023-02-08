8ea2137911028990f56d2834c9ebb2ef81ec9a7d
James-Koduah <jameskoduah171@gmail.com>
2023-02-08 09:19:19 -0800

    9-rectangle.py test commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Inherit from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """subclass of BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialization function"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
