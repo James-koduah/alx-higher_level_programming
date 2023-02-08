14039ba5fd98b2b825c55f11571b4e887daa5b40
James-Koduah <jameskoduah171@gmail.com>
2023-02-08 07:39:05 -0800

    tests/7-base_geometry.txt

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Empyt class"""
+
+
+class BaseGeometry():
+    """Nothing"""
+
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates argument value
+            If ```value``` is not an integer 
+                raise error
+            if ```value``` is less than 1
+                raise error
+            Returns nothing
+        """
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value < 1:
+            raise ValueError(f"{name} must be greater than 0")
