ade897352a58afa37479ddbae7ebeec3ca4760c8
James-Koduah <jameskoduah171@gmail.com>
2023-02-08 07:56:43 -0800

    everything

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -19,5 +19,5 @@ class BaseGeometry():
         """
         if type(value) != int:
             raise TypeError(f"{name} must be an integer")
-        if value < 1:
+        elif value < 1:
             raise ValueError(f"{name} must be greater than 0")
