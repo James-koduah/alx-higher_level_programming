08a74e27bfcf6ce8ff29c367502a13213c3c6f78
James-Koduah <jameskoduah171@gmail.com>
2023-02-08 00:49:25 -0800

    3-to_json_string.py what is happening

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import json
-
 """To a JSON representation"""
+import json
 
 
 def to_json_string(my_obj):
     """to_json_string converts an obj into JSON"""
-    return (json.dumps(my_obj))
+    return json.dumps(my_obj)
