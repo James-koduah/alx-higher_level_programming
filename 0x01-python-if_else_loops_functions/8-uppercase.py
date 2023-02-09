d37ff81d1c3ad4854e1b1351656f80a1822f0fd0
James-Koduah <jameskoduah171@gmail.com>
2023-02-09 01:51:43 -0800

    8-uppercase.py first

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    a = ''
+    for i in str:
+        b = ord(i)
+        if b >= 97 and b <= 122:
+            b = b - 32
+        a += chr(b) 
+    print(a)
