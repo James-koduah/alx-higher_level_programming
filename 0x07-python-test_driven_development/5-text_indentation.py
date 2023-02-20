a4fe39c1fbca68334fb4854d71248b3b5f204128
James-Koduah <jameskoduah171@gmail.com>
2023-02-20 11:19:27 -0800

    task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+'''text_indentation'''
+
+
+def text_indentation(text):
+    '''does something'''
+    b = False
+
+    if type(text) != str:
+        raise TypeError("text must be a string")
+
+    for x in range(0, len(text)):
+        if b is True and text[x] == ' ':
+            b = False
+            continue
+        print(text[x], end="")
+        if text[x] == '.' or text[x] == '?' or text[x] == ':':
+            print('\n')
+            b = True
