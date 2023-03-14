55bc0273bbdafcb1814e1913b835f655c5b5bbf5
James-Koduah <jameskoduah171@gmail.com>
2023-03-14 12:36:01 -0700

    15-groups.sql second

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score, COUNT(score) 'number' FROM second_table GROUP BY score HAVING COUNT(score) > 0;
+SELECT score, COUNT(score) 'number' FROM second_table GROUP BY score HAVING COUNT(score) > 0 ORDER BY score DESC;
