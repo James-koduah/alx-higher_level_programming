9159b2edf8a2a4d966686a286dfba45433e85e87
James Koduah <jameskoduah171@gmail.com>
2023-04-11 14:31:00 +0000

    5-to_integer.js: upgrade to Math.floor

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,6 +3,6 @@
 const args = process.argv;
 
 if (/^\d+$/.test(args[2])) {
-  const num = Number(args[2]);
+  const num = Math.floor(Number(args[2]));
   console.log('My number: ' + num);
 } else console.log('Not a number');
