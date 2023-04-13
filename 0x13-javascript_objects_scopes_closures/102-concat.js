100c4c9c9df054ba1c1373897ee475ba83b80072
James Koduah <jameskoduah171@gmail.com>
2023-04-13 22:20:23 +0000

    102-concat.js: semistandard -fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,12 +3,11 @@
 const fs = require('fs');
 const args = process.argv;
 
-for (i = 2; i < 4; i++) {
-	fs.readFile(args[i], 'utf8', (err, data) => {
-  	if (err) return;
-  	fs.writeFile(args[4], data, { flag: 'a+' }, err => {
-    	if (err);
-  	});
-	});
-
+for (let i = 2; i < 4; i++) {
+  fs.readFile(args[i], 'utf8', (err, data) => {
+    if (err) return;
+    fs.writeFile(args[4], data, { flag: 'a+' }, err => {
+      if (err);
+    });
+  });
 }
