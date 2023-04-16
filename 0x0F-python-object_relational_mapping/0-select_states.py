ddb79ff2102fbfab2d11b92a05b146b17d0fee76
James Koduah <jameskoduah171@gmail.com>
2023-04-16 21:48:08 +0000

    0-select_states.py: first

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+args = sys.argv
+
+db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2], db=args[3],port=3306) 
+cur = db.cursor()
+cur.execute('SELECT * FROM states ORDER BY states.id ASC')
+rows = cur.fetchall()
+for row in rows:
+    print(row)
