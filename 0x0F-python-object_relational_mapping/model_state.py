c9ef0ad76a7e57fd11ab15c50d906e8670ef1d14
James Koduah <jameskoduah171@gmail.com>
2023-04-18 16:38:42 +0000

    model_state.py: changed state to states and added documentation

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
+"""
+My first class model of sqlalchemy
+"""
+
+
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
 
 class State(Base):
-    __tablename__ = 'state'
+    """A state model"""
+    __tablename__ = 'states'
 
     id = Column("id", Integer, primary_key=True)
     name = Column("name", String(128))
