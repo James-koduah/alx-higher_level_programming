688deb6fea1138643275bec4a2bc49af609b12a9
James Koduah <jameskoduah171@gmail.com>
2023-05-05 13:14:25 +0000

    6-peak.py: changed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,15 +6,22 @@
 def find_peak(list_of_integers):
     """Find the peak in a list of integers"""
     length = len(list_of_integers)
-    for i in range(1, length - 1):
-        cur = list_of_integers[i]
-        pre = list_of_integers[i - 1]
-        nex = list_of_integers[i + 1]
-        if pre < cur and nex < cur:
-            return cur
-    for i in range(1, length - 1):
-        cur = list_of_integers[i]
-        pre = list_of_integers[i - 1]
-        nex = list_of_integers[i + 1]
-        if pre == cur and nex == cur:
-            return cur
+
+    if length == 0:
+        return None
+    if length == 1:
+        return list_of_integers[0]
+    if length == 2:
+        return max(list_of_integers)
+
+    half = int(length / 2)
+    current = list_of_integers[half]
+    next = list_of_integers[half + 1]
+    previous = list_of_integers[half - 1]
+
+    if previous < current and current > next:
+        return current
+    elif current < previous:
+        return find_peak(list_of_integers[:half])
+    else:
+        return find_peak(list_of_integers[half + 1:])
