9f3b9142b028d42d4f8c5b176839b89988249785
James Koduah <jameskoduah171@gmail.com>
2023-06-23 14:45:18 +0000

    13-insert_number w

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -5,36 +5,47 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *h = *head;
-	listint_t *hp;
-	int b;
 	listint_t *new = malloc(sizeof(listint_t));
-	if (h == NULL)
-	{	
+	listint_t *next;
+	listint_t *start = *head;
+	int current_int;
+
+	if (*head == NULL)
+	{
+		/*insert into empty list*/
 		new->n = number;
 		new->next = NULL;
 		*head = new;
 		return (*head);
 	}
-	while (h)
+	current_int = (*head)->n;
+	if (current_int >= number)
 	{
-		b = h->n;
-		if (b <= number)
-		{
-			hp = h;
-			h = h->next;
-		}
-		else
-		{
-			break;	
-		}
+		/*insert at start/begining*/
+		new->n = number;
+		new->next = *head;
+		*head = new;
+		return (*head);
 	}
-	if (h == NULL)
-		return (NULL);
-
+	next = (*head)->next;
+	while(next != NULL)
+	{
+		current_int = next->n;
+		if (current_int >= number)
+			break;
+		*head = next;
+		next = (*head)->next;
+	}
+	if (next == NULL)
+	{
+		/*insert at end*/
+		new->n = number;
+		new->next = next;
+		(*head)->next = new;
+		return (start);
+	}
 	new->n = number;
-	new->next = h;
-	hp->next = new;
-
-	return new;
+	new->next = (*head)->next;
+	(*head)->next = new;
+	return (start);
 }
