44682e38a6fea60d288ca5c0c138a5dca0212dc4
James Koduah <jameskoduah171@gmail.com>
2023-07-25 04:58:58 +0000

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-
-
-let args = process.argv.slice(2);
-let fs = require('fs')
-let data = fs.readFileSync(args[0], 'utf-8')
-console.log(data)
+const args = process.argv.slice(2);
+const fs = require('fs');
+const data = fs.readFileSync(args[0], 'utf-8');
+console.log(data);
